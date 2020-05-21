ae55469f062349673b310a9e216633f77d574812
sooryanarayanan <sooryanarayanan@qburst.com>
2020-05-21 20:37:39 +0530

    DFS in graphs

---
+++ CodingNinja/Graphs/dfs.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printDFS(int** edges, int n, int sv, bool* visited){
+
+    cout<<sv<<endl;
+    visited[sv] = true;
+
+    for(int i=0; i<n; i++){
+         if(i == sv)
+            continue;
+         if(edges[sv][i] ==1){
+             if(visited[i])
+                continue;
+             printDFS(edges, n, i, visited);
+    
+        }
+    }
+
+
+}
+
+int main(){
+   
+   int n, e;
+   cin>>n>>e;
+
+   int** edges = new int*[n];
+   for(int i=0; i<n; i++){
+       edges[i] = new int[n];
+       for(int j=0; j<n; j++)
+           edges[i][j] = 0;
+   }
+
+   for(int i=0; i<e; i++){
+       int f,s;
+       cin>>f>>s;
+       edges[f][s] = 1;
+       edges[s][f] = 1;
+   }
+
+   bool* visited = new bool[n];
+   for(int i=0; i<n; i++)
+      visited[i] = 0;
+   printDFS(edges, n, 0, visited);
+
+   
+  for(int i=0; i<n; i++)
+      delete[] edges[i];
+
+   delete[] edges;
+
+   return 0;
+}
